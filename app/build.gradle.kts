9cc185fd906b2c1b2184285d4f1ed2620e7680c9
David Shibru <davshibru@gmail.com>
2025-12-28 11:30:43 +0600

    set up: android's properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.davidshibru.taskflow"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.davidshibru.taskflow"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
