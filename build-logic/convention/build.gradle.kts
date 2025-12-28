e131c09a6a9b07fe394a1a34c8b34caf277d5ab2
David Shibru <davshibru@gmail.com>
2025-12-28 13:09:33 +0600

    add: convention plugins (28.12.2025)

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.davidshibru.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        create("application-convention") {
+            id = "com.davidshibru.convention.application.convention"
+            implementationClass = "com.davidshibru.convention.AndroidApplicationConventionPlugin"
+        }
+        create("compose-convention") {
+            id = "com.davidshibru.convention.compose.convention"
+            implementationClass = "com.davidshibru.convention.ComposeConventionPlugin"
+        }
+    }
+}
